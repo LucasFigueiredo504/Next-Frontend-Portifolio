e7b1507fe8c297c754781ba01d5e327ccee6f83c
lucastheldl <lucastheldl@gmail.com>
2023-09-02 14:25:34 -0300

    fix:fixed white bg

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -17,7 +17,9 @@ export default function RootLayout({
 }) {
   return (
     <html lang="pt-br">
-      <body className={`${roboto.variable} font-sans`}>{children}</body>
+      <body className={`${roboto.variable} font-sans bg-terciary`}>
+        {children}
+      </body>
     </html>
   );
 }
