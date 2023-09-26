1ef59b52907ff71d3cd98d9ec6d389ad3ecc52bd
lucastheldl <lucastheldl@gmail.com>
2023-09-25 22:26:22 -0300

    feat: added new project

--- src/lib/lists.ts
+++ src/lib/lists.ts
@@ -3,7 +3,7 @@ import PokeApi from "../assets/Pokeapi.jpg";
 import tmdb from "../assets/tmdb.png";
 import weather from "../assets/weatherApp.jpg";
 import pomodoro from "../assets/1681137327493.jpg";
-import lore from "../assets/ChaosDragMin.jpg";
+import shirtShop from "../assets/shirtShop.png";
 import miniBlog from "../assets/miniBog.jpg";
 import booksCollection from "../assets/booksCollecion.jpg";
 import placeholder from "../assets/reactLogo.jpg";
@@ -75,48 +75,47 @@ export const skillsList = [
 
 export const projectList = [
   {
-    image: PokeApi,
-    link: "https://lucastheldl.github.io/ReactPokedexV2/",
-    title: "PokeApi",
-    content: "React, Context, local storage, fetch api",
+    image: miniBlog,
+    link: "https://lucastheldl.github.io/React-MiniBlog/",
+    title: "Mini blog",
+    content: "React, Context, Routing, Firebase, Local storage",
   },
   {
-    image: tmdb,
-    link: "https://lucastheldl.github.io/React-TMDB/",
-    title: "TMDB melhores filmes",
-    content: "React, Router, Typescript, fetch api",
-  },
-  {
-    image: weather,
-    link: "https://lucastheldl.github.io/WeatherAPI/",
-    title: "Tempo Agora!",
-    content: "React, fetch api",
+    image: shirtShop,
+    link: "https://shirt-shop-three.vercel.app/",
+    title: "Shirt shop",
+    content: "Next.js, Typescript, Context, Reducer, Stitches CSS, Axios",
   },
   {
     image: pomodoro,
     link: "https://lucastheldl.github.io/IgnitePomodoro/",
     title: "Pomodoro app",
     content:
-      "React, Typescript, Context, Reducer, Styled components, local storage",
+      "React, Typescript, Context, Reducer, Styled components, Local storage",
   },
   {
-    image: miniBlog,
-    link: "https://lucastheldl.github.io/React-MiniBlog/",
-    title: "Mini blog",
-    content: "React, Context, Router, local storage, firebase",
+    image: tmdb,
+    link: "https://lucastheldl.github.io/React-TMDB/",
+    title: "TMDB melhores filmes",
+    content: "React, Router, Typescript, Fetch api",
   },
   {
-    image: lore,
-    link: "https://lucastheldl.github.io/LoreSite/",
-    title: "Lore Artwork site",
-    content: "Javascript, Scss",
+    image: PokeApi,
+    link: "https://lucastheldl.github.io/ReactPokedexV2/",
+    title: "PokeApi",
+    content: "React, Context, Local storage, Fetch api",
   },
-
   {
     image: booksCollection,
     link: "https://lucastheldl.github.io/BookCollection/",
     title: "Book's collection",
-    content: "React, Typescript, Sass, Context, fetch api, local storage",
+    content: "React, Typescript, Sass, Context, Fetch api, Local storage",
+  },
+  {
+    image: weather,
+    link: "https://lucastheldl.github.io/WeatherAPI/",
+    title: "Tempo Agora!",
+    content: "Javascript, Fetch api",
   },
   {
     image: placeholder,
